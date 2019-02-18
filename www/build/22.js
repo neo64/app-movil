95adaa7abbd1c905172355f7b2d74d27e4cfd9b4
Your Name <you@example.com>
2019-02-18 16:19:03 +0100

    modificaciones a la página de perfil

--- www/build/22.js
+++ www/build/22.js
@@ -1,6 +1,6 @@
 webpackJsonp([22],{
 
-/***/ 849:
+/***/ 848:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- www/build/6.js
+++ www/build/6.js
@@ -1,6 +1,6 @@
 webpackJsonp([6],{
 
-/***/ 865:
+/***/ 866:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
