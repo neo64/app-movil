38001a082595bef25b9e7166e17fef6b4c1be61d
Jesús <jesusriobarrilero@gmail.com>
2019-02-18 13:32:46 +0100

    Jesus

---
+++ src/pages/pedir-cita-reserva/pedir-cita-reserva.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PedirCitaReservaPage } from './pedir-cita-reserva';
+
+@NgModule({
+  declarations: [
+    PedirCitaReservaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PedirCitaReservaPage),
+  ],
+})
+export class PedirCitaReservaPageModule {}
